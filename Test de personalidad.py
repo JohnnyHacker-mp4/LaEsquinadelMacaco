d5d836b1558a4fe01e54947bf33be26a17648f13
JohnnyHacker-mp4 <61173494+JohnnyHacker-mp4@users.noreply.github.com>
2020-02-17 22:11:16 -0500

    Update Test de personalidad.py

--- Test de personalidad.py
+++ Test de personalidad.py
@@ -9,7 +9,7 @@ elif p1=="Dinero" or p1=="b" or p1=="b." or p1=="b.dinero" or p1=="dinero":
 elif p1=="Salud" or p1=="c" or p1=="c." or p1=="b.salud" or p1=="salud":
     contador_jm+=1
 
-p2=input("Un conocido suyo le propone un trío con una actriz p*rno, ¿aceptarías?\na.Claro que sí\b.No, nunca lo haría\nc.Todavía soy virgen :(").strip()
+p2=input("Un conocido suyo le propone un trío con una actriz p*rno, ¿aceptarías?\na.Claro que sí\nb.No, nunca lo haría\nc.Todavía soy virgen :(").strip()
 if p2=="a" or p2=="a." or p2=="Sí" or p2=="sí" or p2=="SI" or p2=="si":
     contador_rv+=1
 elif p2=="b" or p2=="b." or p2=="NO" or p2=="no":
